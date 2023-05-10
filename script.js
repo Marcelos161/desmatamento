8e95a8a07bf3f5157c79ad5c1c9e35ce18cee1bf
Marcelo Diogo <marcelos.diogos@outlook.com>
2023-05-10 16:02:31 -0300

    Feito a parte do quem somos

---
+++ script.js
@@ -0,0 +1,25 @@
+var about = document.getElementById('sobre');
+var btn = document.getElementById('about')
+
+function sobre() {
+    if (about.style.marginLeft == '77%') {
+        btn.style.backgroundColor = '#0b472d'
+        btn.style.borderRadius = '20% 20% 0 0'
+
+        about.style.marginLeft = '100%';
+        setTimeout (() => {
+            about.style.display = 'none';
+        },1000)
+    } else {
+        btn.style.backgroundColor = '#0b472d'
+        btn.style.borderRadius = '20% 20% 0 0'
+        about.style.display = 'flex';
+        setTimeout (() => {
+            about.style.marginLeft = '77%';
+        },1)
+
+    }
+
+    btn.style.backgroundColor = '#0b472d'
+    btn.style.borderRadius = '20% 20% 0 0'
+}
